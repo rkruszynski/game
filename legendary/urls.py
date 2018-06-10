d1bf35d2d9d7daa676eb2cbe6fb55b0a6f6b8851
rkruszynski <ryszard.kruszynski@gmail.com>
2018-06-10 02:19:11 +0200

    Add team details

--- legendary/urls.py
+++ legendary/urls.py
@@ -5,10 +5,11 @@ from legendary import views
 urlpatterns = [
     # path('', views.index, name='index'),
     path('create/', views.create, name='create'),
-    path('<int:hero_id>/', views.hero_detail, name='hero_detail'),
+    path('hero/<int:hero_id>/', views.hero_detail, name='hero_detail'),
     path('thanks/', views.thanks, name='thanks'),
     path('add_team/', views.add_team, name='add_team'),
     path('teams/', views.teams_page, name='teams_page'),
+    path('teams/<int:team_id>/', views.team_detail, name='team_detail'),
     path('', views.index, name='index'),
     path('add_mastermind', views.add_mastermind, name='add_mastermind'),
     path('masterminds', views.masterminds_page, name='mastermind_page'),
